3c17dfce722a9f9431f0f0b32c40e183a64e53b4
Martin Graf <martingraf@mail.de>
2017-12-07 11:03:26 +0100

    Main Windows kompatibel definiert
    @erik: print funktion bitte mit ( ) weil Windows is bisschen dumm.

--- preparation/Main.py
+++ preparation/Main.py
@@ -14,38 +14,40 @@ import numpy as np
 import SleepData as sd
 import tools
 
-# can also be a text file or list of paths
-filePath = "../SC4001E0/SC4001E0-PSG.edf"
-hypPath = "../SC4001E0"
+if __name__ == '__main__':
 
-# concerting the given hypnograms to a csv:
-# hypno.convert_hypnograms(hypPath)
+    # can also be a text file or list of paths
+    filePath = "../SC4001E0/SC4001E0-PSG.edf"
+    hypPath = "../SC4001E0"
 
-# read sleep data
-file = sd.SleepData(filePath,
-                    # start = ,
-                    # stop = ,
-                    channels={'EEG': 'EEG Fpz-Cz', 'EMG': 'EMG submental', 'EOG': 'EOG horizontal'},
-                    # preload=False
-                    )
+    # concerting the given hypnograms to a csv:
+    # hypno.convert_hypnograms(hypPath)
 
-eeg = file.get_eeg(0, 6000)
-print file.info()
-eeg = tools.butter_bandpass_filter(eeg, 0.15, 100)
-plt.figure(1)
-plt.subplot(211)
-plt.plot(eeg)
+    # read sleep data
+    file = sd.SleepData(filePath,
+                        # start = ,
+                        # stop = ,
+                        channels={'EEG': 'EEG Fpz-Cz', 'EMG': 'EMG submental', 'EOG': 'EOG horizontal'},
+                        # preload=False
+                        )
 
-eeg = np.fft.fft(eeg)
-plt.subplot(212)
-geloet = (pow(abs(eeg), 2) + np.hanning(6000))
-geloet = geloet[0:3000]
-x_ticks_real = range(0, 3000, 200)
-x_ticks_mod = []
-for i in (range(0, len(x_ticks_real))):
-    x_ticks_mod.append(x_ticks_real[i] / 60)
-print x_ticks_mod
-plt.xticks(x_ticks_real, x_ticks_mod)
-plt.yscale('log')
-plt.plot(geloet)
-plt.show()
+    eeg = file.get_eeg(0, 6000)
+    print (file.info())
+    eeg = tools.butter_bandpass_filter(eeg, 0.15, 100)
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(eeg)
+
+    eeg = np.fft.fft(eeg)
+    plt.subplot(212)
+    geloet = (pow(abs(eeg), 2) + np.hanning(6000))
+    geloet = geloet[0:3000]
+    x_ticks_real = range(0, 3000, 200)
+    x_ticks_mod = []
+    for i in (range(0, len(x_ticks_real))):
+        x_ticks_mod.append(x_ticks_real[i] / 60)
+    print (x_ticks_mod)
+    plt.xticks(x_ticks_real, x_ticks_mod)
+    plt.yscale('log')
+    plt.plot(geloet)
+    plt.show()
