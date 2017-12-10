2494bf8e8d6174b45089c78ca6d45f6765acb3dc
elanfer <elanfer@uos.de>
2017-12-11 00:00:06 +0100

    ada boost and svm added

--- classification/dtree.py
+++ classification/dtree.py
@@ -1,6 +1,7 @@
-from sklearn.cross_validation import train_test_split
 import csv
 
+from sklearn import ensemble
+from sklearn import svm
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
@@ -25,13 +26,24 @@ with open('data/testrun.txt', 'rb') as csvfile:
 x, y = build_arrays(data_array)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=100)
 
-classi_gini = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=3, min_samples_leaf=5)
+classi_gini = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=5, min_samples_leaf=5)
 classi_gini.fit(x_train, y_train)
-classi_gain = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=3, min_samples_leaf=5)
+
+classi_gain = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=5, min_samples_leaf=5)
 classi_gain.fit(x_train, y_train)
+
+classi_svm = svm.SVC()
+classi_svm.fit(x_train, y_train)
+
+classi_ada = ensemble.AdaBoostClassifier(n_estimators=400)
+classi_ada.fit(x_train, y_train)
 
 y_pred_gini = classi_gini.predict(x_test)
 y_pred_gain = classi_gain.predict(x_test)
+y_pred_svm = classi_svm.predict(x_test)
+y_pred_ada = classi_ada.predict(x_test)
 
 print "Gini Accuracy is ", accuracy_score(y_test, y_pred_gini) * 100
 print "Gain Accuracy is ", accuracy_score(y_test, y_pred_gain) * 100
+print "SVM Accuracy is ", accuracy_score(y_test, y_pred_svm) * 100
+print "ADA Accuracy is ", accuracy_score(y_test, y_pred_ada) * 100
