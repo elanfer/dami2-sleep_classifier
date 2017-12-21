bc1e2b259bc26859e5033af1dcbd80d39244631c
Martin Graf <martingraf@mail.de>
2017-12-21 15:14:58 +0100

    normalized power spectra

--- preparation/Main.py
+++ preparation/Main.py
@@ -8,10 +8,11 @@
  pip install PySurfer mne
  python -mpip install -U matplotlib
 """
+import matplotlib.pyplot as plt
 import numpy as np
 
 import feature_extraction as extract
-import tools
+import irasa
 
 if __name__ == '__main__':
     # _________________________________________________________________________________________________________
@@ -22,9 +23,9 @@ if __name__ == '__main__':
     hypPath = "../SC4001E0"
     hypPath_csv = '../SC4001E0/SC4001E0-PSG.edf.csv'
     # start at step (in seconds)
-    start = 0
+    start = 1400
     # use time series data from start until n-windows further (windows of the length win_length in seconds)
-    n_windows = np.nan
+    n_windows = 100  # np.nan
     # classification window length in seconds
     win_length = 30
     # sampling rate
@@ -49,6 +50,39 @@ if __name__ == '__main__':
     # load data
     ts = extract.load_data(filePath, hypnogram=hypnogram)
 
+    spec = np.zeros((2, 32769))
+
+    p1 = []
+    p2 = []
+    freq = []
+
+    for i in range(n_windows):
+        p1 = int(start + i * win_length * s_rate)
+        p2 = int(p1 + win_length * s_rate)
+        [spec_frac, spec_mixed, freq] = irasa.irisa(ts[0][p1:p2], s_rate=s_rate)
+        spec[0, :] = spec[0, :] + spec_frac
+        spec[1, :] = spec[1, :] + spec_mixed
+
+    spec = spec / n_windows
+
+    ts1 = ts[0][p1:p2]
+    us_ts = irasa.myresample(ts1, 1 / 1.9, interp="cubic")
+
+    plt.figure(21)
+    plt.plot(np.linspace(1, 30000, num=len(ts1)), ts1)
+    plt.plot(np.linspace(1, 30000, num=len(us_ts)), us_ts)
+
+    plt.figure(22)
+    plt.title('EEG')
+    plt.grid(True)
+    # plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('Hz')
+    plt.plot(freq, spec[1])
+    plt.plot(freq, spec[0])
+    plt.show()
+
+    '''
     # get basic properties as plots, calculate spindle envelope
     # [spindle_env]
     spindle_env = extract.basic_properties(ts, hypnogram,
@@ -75,3 +109,4 @@ if __name__ == '__main__':
                         hypno_array=hypno_array,
                         win_length=win_length,
                         s_rate=s_rate)
+    '''
